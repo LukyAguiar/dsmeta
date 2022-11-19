00c71bf0d0a10458cc0ab1dfabafa8e9ebcfe8bf
Lucas Aguiar <aguiar@kali.Argonaut>
2022-11-18 21:53:05 -0300

    Adicionando Consulta por Data

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -2,9 +2,13 @@ package com.devsuperior.dsmeta.controllers;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +20,20 @@ public class SaleController {
     private SaleService service;
 
     @GetMapping("/findSales")
-    public List<Sale> findSales(){
-        return  service.findSales();
+    public List<Sale> findSales() {
+        return service.findSales();
+    }
+
+    @GetMapping("/findSalesPage") //Exemplo Pageable
+    public Page<Sale> findSalesPage(Pageable pageable) {
+        return service.findSalesPage(pageable);
+        // Retornando com Exemplo com Page
+    }
+
+    @GetMapping("/findSalesDate")
+    public Page<Sale> findSalesDate(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                    @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
+                                    Pageable pageable) {
+        return service.findSalesDate(minDate, maxDate, pageable);
     }
 }
